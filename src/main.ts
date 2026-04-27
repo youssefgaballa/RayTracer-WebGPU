30bb12e887e852ed753beeedb93b64469605e002
Youssef Gaballa <yg01467@uga.edu>
2026-04-27 17:36:03 -0400

    WebGPU compatability check

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
-import './style.css'
+import "./style.css";
 
+const output_label: HTMLElement = document.getElementById(
+  "compatibility-label",
+) as HTMLElement;
+
+if (navigator.gpu) {
+  output_label.innerText = "WebGPU is supported on this browser";
+} else {
+  output_label.innerText = "WebGPU is not supported on this browser";
+}
