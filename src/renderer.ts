d10e3ccff2947b7aa9f1c292cf39c6dd86791819
Youssef Gaballa <yg01467@uga.edu>
2026-04-28 12:18:10 -0400

    create compute shader ray tracer

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,5 +1,5 @@
-import shader from "./shaders/shaders.wgsl?raw";
-import { Triangle } from "./triangle";
+import textureShader from "./shaders/TextureShader.wgsl?raw";
+import computeShader from "./shaders/computeShader.wgsl?raw";
 
 export class Renderer {
   public isSupported: boolean = true;
@@ -8,16 +8,25 @@ export class Renderer {
   private context!: GPUCanvasContext;
   private textureFormat!: GPUTextureFormat;
 
-  private bindGroupLayout!: GPUBindGroupLayout;
-  private bindGroup!: GPUBindGroup;
-  private triangles: Array<Triangle> = [];
+  private computeShaderModule!: GPUShaderModule;
   private vertexShaderModule!: GPUShaderModule;
   private fragmentShaderModule!: GPUShaderModule;
 
-  private renderPipeline!: GPURenderPipeline;
-  private commandEncoder!: GPUCommandEncoder
+  private colorBuffer!: GPUTexture;
+  private colorBufferView!: GPUTextureView;
+  private sampler!: GPUSampler;
+
+  private computeBindGroupLayout!: GPUBindGroupLayout;
+  private computeBindGroup!: GPUBindGroup;
+  private textureBindGroupLayout!: GPUBindGroupLayout;
+  private textureBindGroup!: GPUBindGroup;
+
+  // Compute pipeline: 
+  private computePipeline!: GPUComputePipeline;
+
+  // Render Pipeline:
+  private textureRenderPipeline!: GPURenderPipeline;
   private textureView!: GPUTextureView;
-  private renderPassEncoder!: GPURenderPassEncoder;
 
   constructor(canvas: HTMLCanvasElement) {
     this.canvas = canvas;
@@ -33,84 +42,183 @@ export class Renderer {
   }
 
   public render() {
-    this.configureRenderPass();
-    
-    this.renderPassEncoder.setPipeline(this.renderPipeline);
-    // set vertex buffers for each triangle
-    this.triangles.forEach((t) => {
-      this.renderPassEncoder.setVertexBuffer(0, t.buffer);
-    })
-    this.renderPassEncoder.setBindGroup(0, this.bindGroup);
-    this.renderPassEncoder.draw(3, 1, 0, 0);
+    // Create Command Encoder to encode commands to be sent to GPU:
+    const commandEncoder: GPUCommandEncoder = this.device.createCommandEncoder({
+      label: "Basic Comamnd Encoder",
+    });
 
-    this.renderPassEncoder.end();
+    // Compute Pass:
+    const computePassEncoder: GPUComputePassEncoder = commandEncoder.beginComputePass();
+    computePassEncoder.setPipeline(this.computePipeline);
+    computePassEncoder.setBindGroup(0, this.computeBindGroup);
+    computePassEncoder.dispatchWorkgroups(
+      // Math.floor((this.canvas.width + 7) / 8),
+      // Math.floor((this.canvas.height + 7) / 8),
+      Math.floor(this.canvas.width),
+      Math.floor(this.canvas.height),
+      1,
+    );
+    computePassEncoder.end();
 
-    this.device.queue.submit([this.commandEncoder.finish()]);
-  }
-
-  private configureRenderPass() {
-    this.commandEncoder = this.device.createCommandEncoder();
-
+    // Texture Render Pass:
     this.textureView = this.context.getCurrentTexture().createView();
-    
-    this.renderPassEncoder = this.commandEncoder.beginRenderPass({
+    const renderPassEncoder = commandEncoder.beginRenderPass({
+      // Render Pass Descriptor
+      label: "Basic RenderPass Encoder",
       colorAttachments: [
         {
-          view: this.textureView,
+          view: this.textureView, //context.getCurrentTexture().createView();
           clearValue: { r: 0.5, g: 0.0, b: 0.25, a: 1.0 },
           loadOp: "clear",
           storeOp: "store",
         },
       ],
     });
+    renderPassEncoder.setPipeline(this.textureRenderPipeline);
+    renderPassEncoder.setBindGroup(0, this.textureBindGroup);
+    renderPassEncoder.draw(6, 2, 0, 0);
+    renderPassEncoder.end();
 
+    this.device.queue.submit([
+      commandEncoder.finish(), // GPUCommandBuffer
+    ]);
+
+    requestAnimationFrame(() => this.render());
   }
 
   private configurePipeline() {
-    const pipelineLayout = this.device.createPipelineLayout({
-      bindGroupLayouts: [this.bindGroupLayout],
+    /*
+      Create Compute Pipeline
+    */
+    const computePipelineLayout = this.device.createPipelineLayout({
+      bindGroupLayouts: [this.computeBindGroupLayout],
     });
 
-    this.renderPipeline = this.device.createRenderPipeline({
-      layout: pipelineLayout,
+    this.computePipeline = this.device.createComputePipeline({
+      layout: computePipelineLayout,
+      compute: {
+        module: this.computeShaderModule,
+        entryPoint: "main",
+      },
+    });
+
+    /*
+      Create Texture Pipeline
+    */
+    const texturePipelineLayout = this.device.createPipelineLayout({
+      bindGroupLayouts: [this.textureBindGroupLayout], // @group(0)
+    });
+
+    this.textureRenderPipeline = this.device.createRenderPipeline({
+      layout: texturePipelineLayout,
       vertex: {
         module: this.vertexShaderModule,
         entryPoint: "vs",
-        // set buffers 
-        buffers: this.triangles.map((t) => t.vertexBufferLayout)
       },
       fragment: {
         module: this.fragmentShaderModule,
         entryPoint: "fs",
-        targets: [{ format: this.textureFormat }],
+        targets: [
+          { format: this.textureFormat }, // @location(0) in fragment shader
+        ],
       },
     });
   }
 
-  private configureBuffers() {
-    const tri: Triangle = new Triangle(this.device);
+  private configureBindGroups() {
+    // Compute Shader Bind Group and Bind Group Layout:
+    this.computeBindGroupLayout = this.device.createBindGroupLayout({
+      entries: [
+        {
+          binding: 0, // @layout(0)
+          visibility: GPUShaderStage.COMPUTE,
+          storageTexture: {
+            access: "write-only",
+            format: "rgba8unorm",
+            viewDimension: "2d",
+          },
+        },
+      ],
+    });
 
-    this.triangles.push(tri);
+    this.computeBindGroup = this.device.createBindGroup({
+      layout: this.computeBindGroupLayout,
+      entries: [
+        {
+          binding: 0, // @layout(0)
+          resource: this.colorBufferView,
+        },
+      ],
+    });
+
+    // Texture Bind Group and Bind Group Layout:
+    this.textureBindGroupLayout = this.device.createBindGroupLayout({
+      entries: [
+        {
+          binding: 0, // @layout(0)
+          visibility: GPUShaderStage.FRAGMENT,
+          sampler: {},
+        },
+        {
+          binding: 1, // @layout(1)
+          visibility: GPUShaderStage.FRAGMENT,
+          texture: {},
+        },
+      ],
+    });
+    this.textureBindGroup = this.device.createBindGroup({
+      layout: this.textureBindGroupLayout,
+      entries: [
+        {
+          binding: 0, // @layout(0)
+          resource: this.sampler,
+        },
+        {
+          binding: 1, // @layout(1)
+          resource: this.colorBufferView,
+        },
+      ],
+    });
   }
 
-  private configureBindGroups() {
-    this.bindGroupLayout = this.device.createBindGroupLayout({
-      entries: [],
+  private configureBuffers() {
+    this.colorBuffer = this.device.createTexture({
+      size: {
+        width: this.canvas.width, // 800 px
+        height: this.canvas.height, // 600 px
+      },
+      format: "rgba8unorm",
+      usage:
+        GPUTextureUsage.COPY_DST |
+        GPUTextureUsage.STORAGE_BINDING |
+        GPUTextureUsage.TEXTURE_BINDING, // storage texture
     });
 
-    this.bindGroup = this.device.createBindGroup({
-      layout: this.bindGroupLayout,
-      entries: [],
-    });
+    this.colorBufferView = this.colorBuffer.createView();
+
+    const samplerDescriptor: GPUSamplerDescriptor = {
+      addressModeU: "repeat",
+      addressModeV: "repeat",
+      magFilter: "linear",
+      minFilter: "nearest",
+      mipmapFilter: "nearest",
+      maxAnisotropy: 1,
+    };
+    this.sampler = this.device.createSampler(samplerDescriptor);
   }
 
   private loadShaders() {
+    this.computeShaderModule = this.device.createShaderModule({
+      label: "Compute Shader",
+      code: computeShader,
+    });
     this.vertexShaderModule = this.device.createShaderModule({
-      code: shader,
+      label: "Vertex Shader",
+      code: textureShader,
     });
     this.fragmentShaderModule = this.device.createShaderModule({
       label: "Fragment Shader",
-      code: shader,
+      code: textureShader,
     });
   }
 
